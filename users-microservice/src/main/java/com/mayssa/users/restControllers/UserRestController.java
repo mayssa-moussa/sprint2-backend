90b549f1d0e92a836d098709773738dc6b90b535
mayssa-moussa <mayssa.moussa565@gmail.com>
2024-10-21 20:51:58 +1200

    commits5

---
+++ users-microservice/src/main/java/com/mayssa/users/restControllers/UserRestController.java
@@ -0,0 +1,21 @@
+package com.mayssa.users.restControllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mayssa.users.entities.User;
+import com.mayssa.users.service.UserService;
+@RestController
+@CrossOrigin(origins = "*")
+public class UserRestController {
+	@Autowired
+	UserService userService;
+	@GetMapping("all")
+	public List<User> getAllUsers() {
+	return userService.findAllUsers();
+	}
+
+}
